e342ff84063d5c93d8e7414bf4804047a19f742b
Luísa Rodrigues Foppa <lulufoppa@gmail.com>
2023-10-30 13:15:49 -0300

    feat: code working

--- Assets/Script/Keycode.cs
+++ Assets/Script/Keycode.cs
@@ -11,6 +11,7 @@ public class Keycode : MonoBehaviour
     [SerializeField] private TMP_Text secondText;
     [SerializeField] private TMP_Text thirdText;
     [SerializeField] private TMP_Text fourthText;
+    [SerializeField] private TMP_Text fithText;
 
     public Button currentButton;
     public TMP_Text currentText;
@@ -18,32 +19,30 @@ public class Keycode : MonoBehaviour
     void Start()
     {
         oneText.text = "0";
-        // secondText.text = "0";
-        // thirdText.text = "0";
-        // fourthText.text = "0";
+        secondText.text = "0";
+        thirdText.text = "0";
+        fourthText.text = "0";
+        fithText.text = "0";
     }
 
     // Update is called once per frame
     public void AddNumber()
     {
         currentButton = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
-        Debug.Log(currentButton);
         currentText = currentButton.GetComponentInChildren<TMP_Text>();
-        Debug.Log(currentText);
-
-    //     int num = int.Parse(currentTextButton.text);
-    //     if(num ==9)
-    //     {
-    //         currentTextButton.text = 0.ToString();
-    //     }
-    //     else{
-    //         currentTextButton.text = (num+1).ToString();
-    //     }
-    //    VerifyCode(currentTextButton);
+        int num = int.Parse(currentText.text);
+        if(num ==9)
+        {
+            currentText.text = 0.ToString();
+        }
+        else{
+            currentText.text = (num+1).ToString();
+        }
+       VerifyCode();
     }
 
-    public void VerifyCode(TMP_Text oneText){
-        if (oneText.text == "9" ){
+    public void VerifyCode(){
+        if (oneText.text == "9" && secondText.text == "3" && thirdText.text == "4" && fourthText.text == "8" && fithText.text == "2"){
             Debug.Log("Certo");
         }else{
             Debug.Log("Errado");
